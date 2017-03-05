0bed72502f4085f695f2b0bc3590bcbfc4a3b3df
josephvong <m15914375662@163.com>
2017-03-05 15:08:41 +0800

    20170305

--- src/components/casegroup/todos/todos.vue
+++ src/components/casegroup/todos/todos.vue
@@ -1,54 +1,73 @@
 <template>
-  <div class="todos">
-    <!-- <h1 class="todo-title">This Is A Todo List</h1> -->
+  <div class="todos"> 
     <div class="todo-input">
       <group>
-        <x-input title="事件名称" placeholder="填写事件名" ></x-input>
-        <x-textarea :max="30" placeholder="请填写详细信息"></x-textarea>
+        <flexbox>
+            <flexbox-item :span="9">
+                <x-input title="事件名称" placeholder="填写事件名" ></x-input>
+            </flexbox-item>
+            <flexbox-item :span="2">
+                 <x-button mini type="primary" v-on:click.native="">提交</x-button>
+            </flexbox-item>
+        </flexbox>
+        <x-textarea :max="30" placeholder="请填写详细信息"  ></x-textarea>
       </group>
     </div>
-    <div>
+    <!-- <todo-tab></todo-tab>  -->
+    <div class="todo-tab">
       <tab>
-        <tab-item selected>新事件</tab-item>
-        <tab-item>重要事</tab-item>
-        <tab-item>已完成</tab-item>
+        <tab-item :selected="tabState==0" v-on:click.native="tabChange(0)">新事件</tab-item>
+        <tab-item :selected="tabState==1" v-on:click.native="tabChange(1)">已完成</tab-item>
+        <tab-item :selected="tabState==2" v-on:click.native="tabChange(2)">已取消</tab-item>
       </tab>
-      <div style="height:200px;border-bottom:1px solid red">
-      <swiper>
-        <swiper-item >
-          <div style="height:200px;background:blue">aaa</div>
-        </swiper-item>
-        <swiper-item >
-          BBB
-        </swiper-item>
-        <swiper-item >
-          CCC
-        </swiper-item>
-      </swiper>
-      </div>
-    </div>
+    </div> 
+    <div class="todo-body">
+      <todo-body :tabState="tabState"></todo-body> 
+       
+    </div> 
+    
   </div>
 </template>
 
 <script>
-import { Group,CellBox,Cell,XInput,XTextarea ,Tab ,TabItem,Swiper,SwiperItem} from 'vux'
+import {mapState , mapMutations } from 'vuex'
+import todoBody from "./todoBody"
+import {  Group, XInput,XTextarea ,Flexbox,FlexboxItem,XButton,Tab,TabItem } from 'vux'
 export default {
-  name: 'caseGroup',
+  name: 'todos',
   data () {
     return {
-
+       
     }
   },
+  computed: mapState({
+    tabState: state=>state.todoM.tabState
+    
+  }),
   components:{
-    Group,CellBox,Cell,XInput,XTextarea,Tab ,TabItem,Swiper,SwiperItem
+  todoBody, 
+    Group, XInput,XTextarea,Flexbox,FlexboxItem,XButton,Tab,TabItem
+  },
+  methods:{
+   // ...mapActions({tabChange:"todoTabChange",N})
+   tabChange(N){
+    //alert(N)
+    this.$store.dispatch("todoTabChange",N) 
+   }
+
+
+  },
+  mounted(){ 
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="stylus" rel="stylesheet/stylus" >
-.todo-title
-  width:100%
-  height:1rem
-  text-align:center
+.todo-body
+  position:absolute
+  left:0
+  right:0
+  bottom:0
+  top:14rem 
 </style>
--- src/vuex/appstore.js
+++ src/vuex/appstore.js
@@ -6,10 +6,28 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
+const todoModule={
+	state:{
+		tabState:0, // tab 的显示状态，0/1/2 
+		eventObject:{title:"",content:"",eventID:""}
+	},
+	mutations:{
+		todoTabChange(state,num){ 
+			state.tabState=num;
+		}
+	},
+	actions:{
+		todoTabChange(context,num){
+			context.commit('todoTabChange',num)
+		}
+	}
+}
+
 let store =new Vuex.Store({
-  state:{
-  	count:0
-  }
+   modules:{
+   		todoM:todoModule
+   },
+  
 })
 
 export default store
